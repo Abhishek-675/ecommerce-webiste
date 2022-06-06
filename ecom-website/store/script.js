77428140a50b27e1b7923d8f5b9a33b6eb59c7e6
Abhishek <dekaa331@gmail.com>
2022-06-06 14:41:23 +0530

    task 11 added

--- ecom-website/store/script.js
+++ ecom-website/store/script.js
@@ -1,19 +1,19 @@
 var cartBtn = document.getElementById('cart-top');
 
 cartBtn.addEventListener('click', () => {
-    const sideCart= document.getElementById('cart-float');
+    const sideCart = document.getElementById('cart-float');
     sideCart.style.display = 'block';
 });
 
 var cartClose = document.getElementById('cancel');
 
 cartClose.addEventListener('click', () => {
-    const sideCart= document.getElementById('cart-float');
+    const sideCart = document.getElementById('cart-float');
     sideCart.style.display = 'none';
 });
 
 
-const parentContainer= document.getElementById('container-parent');
+const parentContainer = document.getElementById('container-parent');
 const addBtn = document.getElementById('add-btn');
 
 
@@ -21,26 +21,27 @@ parentContainer.addEventListener('click', (e) => {
     const cartItems = document.getElementById('cart-items');
 
     if (e.target.id === 'add-btn') {
-        const id = e.target.parentNode.id; 
-        const name = document.querySelector(`#${id} h4`).innerText;
-        const image = document.querySelector(`#${id} img`).src;
-        const price = document.querySelector(`#${id} label`).innerText;
+        const id = e.target.parentNode.id;
+        // console.log(id)
+        const name = document.getElementById(`${id}-h4`).innerText;
+        const image = document.getElementById(`${id}-img`).src;
+        const price = document.getElementById(`${id}-label`).innerText;
 
         if (document.querySelector(`#in-cart-${id}`)) {
             alert('Item is already in the cart');
             return;
         }
-        
-        const div =  document.createElement('div');
+
+        const div = document.createElement('div');
         div.setAttribute('class', 'cart-div');
         div.setAttribute('id', `in-cart-${id}`);
-        div.innerHTML= `
+        div.innerHTML = `
         <span><img class='cart-class-img' src=${image}>
         <span>${name}</span></span>  
         <span>${price}</span>
         <span><input type='text' value='1'>
         <button id='cart-remove-btn'>REMOVE</button></span>`
-       
+
         cartItems.append(div);
 
 
@@ -58,4 +59,30 @@ parentContainer.addEventListener('click', (e) => {
         }, 1000);
     }
 })
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    axios.get('http://localhost:3000/products')
+        .then(response => {
+            console.log(response.data);
+            for (var i=0; i<response.data.length; i++) {
+                showProducts(response.data[i]);
+            };
+        })
+        .catch(err => console.log(err));
+});
+
+function showProducts(product) {
+    
+    const div = document.getElementById('product');
+    div.setAttribute('id', `${product.id}`);
+    div.innerHTML = `
+    <h4 id='${product.id}-h4'>${product.title}</h4>
+    <img id='${product.id}-img' src="${product.imageUrl}"
+        alt="album cover">
+    <br>
+    <label id='${product.id}-label'>${product.price}</label>
+    <button class="add-btn" id="add-btn">ADD TO CART</button>`;
+}
 
